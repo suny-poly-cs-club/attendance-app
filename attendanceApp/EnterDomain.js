b28c452a4065496cefe87e26ed3fd72c386b9c1c
SvenArcticFox <74692162+SvenArcticFox@users.noreply.github.com>
2024-02-27 13:53:49 -0500

    Update EnterDomain.js

--- attendanceApp/EnterDomain.js
+++ attendanceApp/EnterDomain.js
@@ -1,17 +1,38 @@
 import {
   Text,
   View,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+  Button
 } from 'react-native';
 
 const EnterDomain = ({navigation}) => {
-	
+
 	return (
 		<View>
 			<Text>
-				ENTER PAGE CONTENT HERE
+			    Enter Your Domain:
 			</Text>
+      <View>
+        <TextInput styles={styles.input} placeholder="Your domain"/>
+      </View>
+      <View>
+        <Button
+        title="Enter" />
+
+      </View>
 		</View>
 	);
 };
 
+const styles = StyleSheet.create({
+  input: {
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    width: "100%",
+  }
+})
 export default EnterDomain;
