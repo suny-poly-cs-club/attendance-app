c0a8afcdedc3474cf3f9ac0b2125462f098f4106
jSdCool <jSdCoolgaming@gmail.com>
2024-02-27 13:53:28 -0500

    (qr scanner) android

--- attendanceApp/Scan.js
+++ attendanceApp/Scan.js
@@ -1,17 +1,55 @@
 import {
   Text,
   View,
+  StyleSheet,
+  TouchableOpacity,
 } from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import { RNCamera } from 'react-native-camera';
 
 const Scan = ({navigation}) => {
 	
 	return (
 		<View>
-			<Text>
-				ENTER PAGE CONTENT HERE
-			</Text>
+			<QRCodeScanner
+				onRead={this.onSuccess}
+				flashMode={RNCamera.Constants.FlashMode.flash}
+				topContent={
+					<Text style={styles.centerText}>
+						Go to{' '}
+						<Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+						your computer and scan the QR code.
+					</Text>
+				}
+				bottomContent={
+					<TouchableOpacity style={styles.buttonTouchable}>
+						<Text style={styles.buttonText}>OK. Got it!</Text>
+					</TouchableOpacity>
+				}
+			/>
 		</View>
 	);
 };
+
+
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777'
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000'
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)'
+  },
+  buttonTouchable: {
+    padding: 16
+  }
+});
 
 export default Scan;
