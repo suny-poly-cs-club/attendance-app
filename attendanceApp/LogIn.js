82a4a65188a8b0647bd2439049189ccdb3d50653
Janan Malone <malonejp@sunypoly.edu>
2024-02-28 16:14:19 -0500

    Working on Login screen - JPM

--- attendanceApp/LogIn.js
+++ attendanceApp/LogIn.js
@@ -1,17 +1,40 @@
 import {
   Text,
   View,
+  TextInput,
+  TouchableOpacity 
 } from 'react-native';
+import { useState } from 'react';
 
 const LogIn = ({navigation}) => {
-	
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 	return (
-		<View>
-			<Text>
-				ENTER PAGE CONTENT HERE
-			</Text>
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+		<Text style={{ fontSize: 20, marginBottom: 20 }}>WELCOME</Text>
+		<Text style={{ marginBottom: 10 }}>Please log in below</Text>
+		  <TextInput
+			style={{ borderWidth: 1, padding: 10, marginBottom: 15, width: 250 }}
+			placeholder="Username"
+			value={username}
+			onChangeText={setUsername}
+		  />
+		  <TextInput
+			style={{ borderWidth: 1, padding: 10, marginBottom: 15, width: 250 }}
+			placeholder="Password"
+			value={password}
+			onChangeText={setPassword}
+			secureTextEntry={true} 
+		  />
+		  <TouchableOpacity
+			style={{ backgroundColor: 'blue', padding: 10 }}
+			onPress={() => handleLogin(username, password)}
+		  >
+			<Text style={{ color: 'white' }}>Log In</Text>
+		  </TouchableOpacity>
 		</View>
-	);
-};
+	  );
+	};
+	
 
 export default LogIn;
