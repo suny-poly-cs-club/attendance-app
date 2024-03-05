2a27953dc915da912d2700db61687d025ce60cce
jSdCool <jSdCoolgaming@gmail.com>
2024-03-05 13:55:57 -0500

    start on domain listing

--- attendanceApp/DomainList.js
+++ attendanceApp/DomainList.js
@@ -1,15 +1,34 @@
+import React, { useState } from 'react';
 import {
   Text,
   View,
+  FlatList,
 } from 'react-native';
 
+
+
+let v2 = false;
+
+	
+
 const DomainList = ({navigation}) => {
+	const [domains, setDomains] = useState([]);
+	if(!v2){
+		setDomains(["HI","HOW","EEEEEE"]);
+		v2=true;
+	}
+	
 	
 	return (
 		<View>
 			<Text>
-				ENTER PAGE CONTENT HERE
+				Select Domain:
 			</Text>
+			
+			 <FlatList
+			 data = {domains}
+			 renderItem={({item}) => <Text> {item} </Text>}
+			/>
 		</View>
 	);
 };
