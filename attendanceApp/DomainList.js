0ee6c17691d568a25e8c24b8fc24cbb742ded99b
jSdCool <37940266+jSdCool@users.noreply.github.com>
2024-03-06 00:03:54 -0500

    Update DomainList.js

--- attendanceApp/DomainList.js
+++ attendanceApp/DomainList.js
@@ -3,7 +3,16 @@ import {
   Text,
   View,
   FlatList,
+  useColorScheme,
+  SafeAreaView,
+  Button,
+  StyleSheet,
+  ScrollView,
 } from 'react-native';
+
+import {
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
 
 
 
@@ -11,8 +20,12 @@ let v2 = false;
 
 const DomainItem = ({domain}) => {
 	return (
-		<View>
+		<View style = {styles.listItems}>
 			<Text> {domain} </Text>
+			<View style = {styles.listButtons}>
+				<Button title = "Select" onPress={() => { console.log("EEE")}} />
+				<Button title = "Remove" onPress={() => { console.log("EEE")}} color = "red"/>
+			</View>
 		</View>
 	);
 }
@@ -20,25 +33,53 @@ const DomainItem = ({domain}) => {
 	
 
 const DomainList = ({navigation}) => {
+	const isDarkMode = useColorScheme() === 'dark';
+
+	const backgroundStyle = {
+		backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+	};
+	
 	const [domains, setDomains] = useState([]);
 	if(!v2){
-		setDomains(["HI","HOW","EEEEEE"]);
+		setDomains(["HI","HOW","EEEEEE","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]);
 		v2=true;
 	}
 	
-	
+	//apperently FlatList should have a scroll view inside it but it does not seem to be working for me
+	//apperently you should not put listses inside of scroll views
 	return (
-		<View>
-			<Text>
-				Select Domain:
-			</Text>
-			
-			 <FlatList
-			 data = {domains}
-			 renderItem={({item}) => <Text> {item} </Text>}
-			/>
-		</View>
+		<SafeAreaView style={backgroundStyle}>
+			<ScrollView
+			contentInsetAdjustmentBehavior="automatic"
+			style={backgroundStyle}>
+				<Text>
+					Select Domain:
+				</Text>
+				
+				<FlatList
+				data = {domains}
+				renderItem={({item}) => <DomainItem domain={item} />}
+				contentInsetAdjustmentBehavior="automatic"
+				/>
+			</ScrollView>
+		</SafeAreaView>
 	);
 };
+
+const styles = StyleSheet.create({
+  listItems: {
+    flex: 1,
+	alignItems: 'center',
+	flexDirection: 'row',
+	justifyContent: 'space-between',
+	padding: '1%',
+  },
+  listButtons: {
+    flex: 1,
+	alignItems: 'center',
+	flexDirection: 'row',
+	justifyContent: 'flex-end',
+  }
+});
 
 export default DomainList;
