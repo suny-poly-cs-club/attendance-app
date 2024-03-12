2db20ff872d456634a578a5e674a10b86487ef8e
jSdCool <jSdCoolgaming@gmail.com>
2024-03-12 13:21:04 -0400

    start on sign or login

--- attendanceApp/SignOrLogIn.js
+++ attendanceApp/SignOrLogIn.js
@@ -1,17 +1,32 @@
 import {
   Text,
   View,
+  StyleSheet,
 } from 'react-native';
 
+import React, { useState } from 'react';
+
 const SignOrLogIn = ({navigation}) => {
+	const [orMsg, setOrgMsg] = useState("");
 	
 	return (
 		<View>
-			<Text>
-				ENTER PAGE CONTENT HERE
+			<Text style={styles.title}>
+				SIGN UP {'\n'} OR {'\n'} LOG IN
 			</Text>
 		</View>
 	);
 };
+
+const styles = StyleSheet.create({
+  mainBox: {
+    fontWeight: '700',
+  },
+  title: {
+	fontSize: 30,
+	textAlign: "center"
+  }
+});
+
 
 export default SignOrLogIn;
