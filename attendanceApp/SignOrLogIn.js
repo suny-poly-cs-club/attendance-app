7de031466dc079c69d721d18d9c8f7e3e4158e94
jSdCool <jSdCoolgaming@gmail.com>
2024-03-12 13:40:07 -0400

    finish sign ot long in page

--- attendanceApp/SignOrLogIn.js
+++ attendanceApp/SignOrLogIn.js
@@ -2,19 +2,44 @@ import {
   Text,
   View,
   StyleSheet,
+  Button,
+  ScrollView,
 } from 'react-native';
 
-import React, { useState } from 'react';
+import React, { useState, useEffect  } from 'react';
+
+function goToSignUpPage(){
+	console.log("GO TO SIGN UP");
+}
+
+function goToLoginPage(){
+	console.log("GO TO LOG IN");
+}
 
 const SignOrLogIn = ({navigation}) => {
-	const [orMsg, setOrgMsg] = useState("");
+	const [orgMsg, setOrgMsg] = useState("");
+	
+	useEffect(() => {
+		//load org message from the server here
+		let orgmsg = "TMP MESSAGE";
+		setOrgMsg(orgmsg);
+	},[]);
 	
 	return (
-		<View>
+		<ScrollView
+        contentInsetAdjustmentBehavior="automatic">
 			<Text style={styles.title}>
 				SIGN UP {'\n'} OR {'\n'} LOG IN
 			</Text>
-		</View>
+			<Text> {'\n'} </Text>
+			<Text style={styles.orgMsgStyle}> {orgMsg} </Text>
+			<Text> {'\n'} </Text>
+			<View style = {styles.buttonBox}>
+				<Button title="Log In" onPress = {goToLoginPage} />
+				<Text>{'\n'}</Text>
+				<Button title="Sign Up" onPress = {goToSignUpPage}/>
+			</View>
+		</ScrollView>
 	);
 };
 
@@ -25,6 +50,13 @@ const styles = StyleSheet.create({
   title: {
 	fontSize: 30,
 	textAlign: "center"
+  },
+  orgMsgStyle: {
+	textAlign: "center"  
+  },
+  buttonBox: {
+	  padding: "1%"
+
   }
 });
 
